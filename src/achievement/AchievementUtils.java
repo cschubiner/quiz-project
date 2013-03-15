7e5e8022d34702c5216b13bcce788da1ad4c7b0d
Lingtong Sun <lingtongsun@gmail.com>
2013-03-14 23:11:00 -0700

    remove comment

--- src/achievement/AchievementUtils.java
+++ src/achievement/AchievementUtils.java
@@ -65,7 +65,6 @@ public class AchievementUtils {
 				num = r.getInt(1);
 			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return !(num==0);
